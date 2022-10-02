132e1f3fa2c8b066217fc529c084530ff002c33e
timechain1 <andrey.lutsiak@gmail.com>
2022-10-02 22:04:55 +0300

    completed the second one item

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const partsForInsert = ingredients.map(ingredient => {
+  const currentItem = document.createElement('li');
+  currentItem.textContent = ingredient;
+  currentItem.classList.add('item');
+  return currentItem;
+});
+
+const listRef = document.querySelector('#ingredients');
+listRef.append(...partsForInsert);
